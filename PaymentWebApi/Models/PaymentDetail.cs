8dd7abce9acd52067b219b435300c6caf518adcc
alpesh.choubisa <alpesh.choubisa@accenture.com>
2019-06-02 01:46:07 +0530

    Dot Net Core Payment Crud Application with Angular 5 and Web Api

---
+++ PaymentWebApi/Models/PaymentDetail.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentWebApi.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string CardExpirationDate { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVVNumber { get; set; }
+
+
+    }
+}
